7cae41e036f5ac6afedae5a416c946c2675c65c3
Nevri <vivintejada@gmail.com>
2025-08-13 23:08:59 +0800

    Staged Revisions (IDK kung success na ba) 08/13/25

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import powerImage from '../assets/photos/power.png';
 import fullgirlImage from '../assets/photos/fullgirl.png';
 import info3Image from '../assets/photos/info3.png';
@@ -8,72 +8,164 @@ import redgirlImage from '../assets/photos/redgirl.png';
 import paperImage from '../assets/photos/paper.png';
 
 const Expertise = () => {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="section3 color1" id='expertise'>
       <div className="container">
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-3">
-              <img 
-                src={powerImage} 
-                className="rounded img-fluid" 
-                alt="Power" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
-              <img 
-                src={fullgirlImage} 
-                className="rounded img-fluid" 
-                alt="Full girl" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
+          {/* Mobile layout - controlled by CSS media queries */}
+          <div className="mobile-expertise-layout">
+            <div className="row">
+              <div className="col-12 text-center mb-4">
+                <h1 className="fontcolor2 font-weight-bolder font1 expertiseloc mobile-expertise-title" data-aos="zoom-out-left" data-aos-duration="2000">
+                  EXPERTISE
+                </h1>
+              </div>
             </div>
-            <div className="col-lg-3">
-              <h1>&nbsp;</h1>
-              <img 
-                src={info3Image} 
-                className="rounded img-fluid" 
-                alt="Info 3" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
-              <img 
-                src={designImage} 
-                className="rounded img-fluid" 
-                alt="Design" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
+            <div className="row mobile-expertise-grid">
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={powerImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Power" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={fullgirlImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Full girl" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={info3Image} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Info 3" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={designImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Design" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={fanartImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Fan art" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={redgirlImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Red girl" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-6 mobile-expertise-item">
+                <img 
+                  src={paperImage} 
+                  className="rounded img-fluid mobile-expertise-image" 
+                  alt="Paper" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              {/* Empty cell to complete the 4x2 grid */}
+              <div className="col-6 mobile-expertise-item">
+                {/* You can add another image here or leave empty */}
+              </div>
             </div>
-            <div className="col-lg-3" style={{ marginTop: '15%', fontWeight: '10px' }}>
-              <h1 className="fontcolor2 font-weight-bolder font1 expertiseloc" data-aos="zoom-out-left" data-aos-duration="2000">
-                EXPERTISE
-              </h1>
-              <h1>&nbsp;</h1>
-              <img 
-                src={fanartImage} 
-                className="rounded img-fluid" 
-                alt="Fan art" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
-              <img 
-                src={redgirlImage} 
-                className="rounded img-fluid" 
-                alt="Red girl" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
-            </div>
-            <div className="col-lg-3" style={{ marginTop: '45%' }}>
-              <img 
-                src={paperImage} 
-                className="rounded img-fluid" 
-                alt="Paper" 
-                data-aos="flip-left" 
-                data-aos-duration="2000"
-              />
+          </div>
+
+          {/* Desktop layout - controlled by CSS media queries */}
+          <div className="desktop-expertise-layout">
+            <div className="row">
+              <div className="col-lg-3">
+                <img 
+                  src={powerImage} 
+                  className="rounded img-fluid" 
+                  alt="Power" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+                <img 
+                  src={fullgirlImage} 
+                  className="rounded img-fluid" 
+                  alt="Full girl" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-lg-3">
+                <h1>&nbsp;</h1>
+                <img 
+                  src={info3Image} 
+                  className="rounded img-fluid" 
+                  alt="Info 3" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+                <img 
+                  src={designImage} 
+                  className="rounded img-fluid" 
+                  alt="Design" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-lg-3" style={{ marginTop: '15%', fontWeight: '10px' }}>
+                <h1 className="fontcolor2 font-weight-bolder font1 expertiseloc" data-aos="zoom-out-left" data-aos-duration="2000">
+                  EXPERTISE
+                </h1>
+                <h1>&nbsp;</h1>
+                <img 
+                  src={fanartImage} 
+                  className="rounded img-fluid" 
+                  alt="Fan art" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+                <img 
+                  src={redgirlImage} 
+                  className="rounded img-fluid" 
+                  alt="Red girl" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
+              <div className="col-lg-3" style={{ marginTop: '45%' }}>
+                <img 
+                  src={paperImage} 
+                  className="rounded img-fluid" 
+                  alt="Paper" 
+                  data-aos="flip-left" 
+                  data-aos-duration="2000"
+                />
+              </div>
             </div>
           </div>
         </div>
